99b820c414069dab18fd7950569c988c1cae6e5d
gpalencia <gpalencia@google.com>
2023-07-27 14:57:40 -0400

    pokemon api usage and display pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) {}
+
+  pokemon:any
+
+  title = 'pokemon-api';
+
+  url=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+
+
+  ngOnInit(){ 
+    let headers = new HttpHeaders({
+      'x-rapidapi-host': 'random-facts2.p.rapidapi.com',
+			'x-rapidapi-key': 'your-api-key'
+    })
+    this.http
+        .get<any>(' https://pokeapi.co/api/v2/pokemon?offset=0&limit=151', {
+          headers: headers
+        })
+        .subscribe(pokemonData => {
+          this.pokemon = pokemonData
+          console.log(pokemonData.results)
+        })
+  }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
